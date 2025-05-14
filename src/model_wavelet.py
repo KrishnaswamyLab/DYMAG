ca7b964038342ebfcb4d6840541f2a49074a5b5e
Xingzhi Sun <xingzhi.sun@yale.edu>
2025-05-14 17:36:44 -0400

    rename para

--- src/model_wavelet.py
+++ src/model_wavelet.py
@@ -5,14 +5,14 @@ from torch_geometric.nn import global_mean_pool
 from src.wavelet_conv import WaveletConv
 
 class Wavelet(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2, mean_pool_between_layers=False):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers=2, avg_wvlt_scales=False):
         super().__init__()
         self.in_proj = Linear(in_channels, hidden_channels)
         self.convs = ModuleList()
         self.mlps = ModuleList()
-        self.mean_pool_between_layers = mean_pool_between_layers
-        reshape = not mean_pool_between_layers
-        n_wavelets = 1 if mean_pool_between_layers else 6
+        self.avg_wvlt_scales = avg_wvlt_scales
+        reshape = not avg_wvlt_scales
+        n_wavelets = 1 if avg_wvlt_scales else 6
         self.convs.append(WaveletConv(reshape=reshape))
         self.mlps.append(Sequential(
             Linear(hidden_channels * n_wavelets, hidden_channels), 
@@ -35,7 +35,7 @@ class Wavelet(torch.nn.Module):
         for conv, mlp in zip(self.convs, self.mlps):
             x = conv(x, edge_index) # Shape: [num_nodes, features, num_wavelets]
             x = F.relu(x)
-            if self.mean_pool_between_layers:
+            if self.avg_wvlt_scales:
                 x = x.mean(dim=-1) # take average over the wavelets, Shape: [num_nodes, features]
             x = mlp(x)
         x = global_mean_pool(x, batch)
